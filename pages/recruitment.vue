be1e9644c49e6d4016260637503ca4e8d7bb4feb
ejl242 <ejl242@cornell.edu>
2021-08-20 19:33:08 -0500

    Recruitment refactor + breakpoints

--- pages/recruitment.vue
+++ pages/recruitment.vue
@@ -4,19 +4,11 @@
           {{ data.title }}
       </p>
       <div class="mx-16 my-8">
-          <p class="body-1 font-weight-light mx-8">
+          <p class="body-1 font-weight-light mx-md-8">
           {{ data.description }}
           </p>
-          <div v-for="item in data.info">
-            <v-sparkline :line-width="0.5" :gradient="['grey']" :height="10" :padding="2" :smooth="10" :value="[1, 1]" auto-draw class="mb-4"></v-sparkline>
-            <v-row class="mt-4">
-                <v-col cols="12" md="4">
-                    <p class="flex-grow-0 text-h4 font-weight-medium mx-8 text-uppercase primary--text"> {{ item.title }} </p>
-                </v-col>
-                <v-col cols="12" md="8">
-                    <p v-html="item.body"></p>
-                </v-col>
-            </v-row>
+          <div v-for="item in data.info" v-bind:key="item.title">
+            <Block v-bind:title="item.title"><p v-html="item.body"></p></Block>
           </div>
       </div>
   </div>
