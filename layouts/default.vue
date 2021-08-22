b45cfb098cad9bd748341353e0d99001969a4258
ejl242 <ejl242@cornell.edu>
2021-08-21 19:14:14 -0500

    Updated Navbar
    - Mobile Friendly
    - Team dropdown menu
    - Removed Home button (redundant)
    - smaller

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,6 @@
 <template>
   <v-app >
-    <v-app-bar
-      app
-    >
+    <v-app-bar>
       <NuxtLink to="/">
         <img src="logo.png" width="50px" class="mr-5"/>
       </NuxtLink>
@@ -11,14 +9,112 @@
       </NuxtLink>
       <v-spacer />
 
-      <div v-for="page in pages" :key="page.name">
-        <v-btn :plain="page.plain" :text="page.text" class="nav-btn mx-2 body-2 font-weight-medium" active-class="primary--text" :to="page.to" nuxt>
-          {{page.name}}
+      <!-- Full Nav Bar -->
+      <div class="hidden-sm-and-down">
+        <v-btn plain small class="nav-btn mx-2 body-2 font-weight-medium" active-class="primary--text" :to="'/recruitment'" nuxt>
+          Recruitment
+        </v-btn>
+
+        <v-menu
+          open-on-hover
+          bottom
+          offset-y
+        >
+          <template v-slot:activator="{ on }">
+            <v-btn plain small class="nav-btn mx-2 body-2 font-weight-medium" active-class="primary--text" :to="'/teams'" v-on="on" nuxt>
+              Teams
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="(item, index) in teams" :key="index">
+              <v-list-item-title>
+                <v-btn plain class="nav-btn mx-2 body-2 font-weight-medium" active-class="primary--text" v-bind:to="item.to" v-bind="attrs" v-on="on" nuxt>
+                  {{ item.title }}
+                </v-btn>
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+
+        <v-btn plain small class="nav-btn mx-2 body-2 font-weight-medium" active-class="primary--text" :to="'/people'" nuxt>
+          People
+        </v-btn>
+        <v-btn plain small class="nav-btn mx-2 body-2 font-weight-medium" active-class="primary--text" :to="'/education'" nuxt>
+          Education
+        </v-btn>
+        <v-btn plain small class="nav-btn mx-2 body-2 font-weight-medium" active-class="primary--text" :to="'/blog'">
+          Blog
+        </v-btn>
+        <v-btn text small class="nav-btn mx-2 accent body-2 font-weight-medium" to="/contact" nuxt>
+          Contact
         </v-btn>
       </div>
-      <v-btn text class="nav-btn mx-2 accent body-2 font-weight-medium" to="/contact" nuxt>
-        Contact
-      </v-btn>
+
+      <!-- Mobile Nav Bar -->
+      <v-menu open-on-click bottom offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn plain class="hidden-md-and-up nav-btn body-2 font-weight-medium" active-class="primary--text" v-on="on">
+            <v-icon large >mdi-menu</v-icon>
+          </v-btn>
+        </template>
+        
+        <v-list nav>
+          <v-list-item>
+            <v-btn plain x-small class="nav-btn body-2 font-weight-medium" active-class="primary--text" :to="'/recruitment'" nuxt>
+              Recruitment
+            </v-btn>
+          </v-list-item>
+
+          <v-list-item>
+            <v-menu offset-y>
+              <template v-slot:activator="{ on }">
+                <v-btn plain x-small class="nav-btn body-2 font-weight-medium" active-class="primary--text" v-on="on">
+                  Teams
+                </v-btn>
+              </template>
+
+              <v-list nav>
+                <v-list-item>
+                  <v-list-item-title>
+                    <v-btn plain x-small class="nav-btn body-2 font-weight-medium" active-class="primary--text" v-bind:to="'/teams'" v-bind="attrs" v-on="on" nuxt>
+                      Overview
+                    </v-btn>
+                  </v-list-item-title>
+                </v-list-item>
+                <v-list-item v-for="(item, index) in teams" :key="index">
+                  <v-list-item-title>
+                    <v-btn plain x-small class="nav-btn body-2 font-weight-medium" active-class="primary--text" v-bind:to="item.to" v-bind="attrs" v-on="on" nuxt>
+                      {{ item.title }}
+                    </v-btn>
+                  </v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-list-item>
+
+          <v-list-item>
+            <v-btn plain x-small class="nav-btn body-2 font-weight-medium" active-class="primary--text" :to="'/people'" nuxt>
+              People
+            </v-btn>
+          </v-list-item>
+          <v-list-item>
+            <v-btn plain x-small class="nav-btn body-2 font-weight-medium" active-class="primary--text" :to="'/education'" nuxt>
+              Education
+            </v-btn>
+          </v-list-item>
+          <v-list-item>
+            <v-btn plain x-small class="nav-btn body-2 font-weight-medium" active-class="primary--text" :to="'/blog'">
+              Blog
+            </v-btn>
+          </v-list-item>
+          <v-list-item>
+            <v-btn text x-small class="nav-btn accent body-2 font-weight-medium" to="/contact" nuxt>
+              Contact
+            </v-btn>
+          </v-list-item>
+
+        </v-list>
+      </v-menu>
     </v-app-bar>
 
     <v-main my-5>
@@ -76,45 +172,29 @@ export default {
   data () {
     return {
       hover: false,
-      pages: [
+      title: 'Cornell Data Science',
+      teams: [
         {
-          name: 'Home',
-          to: '/',
-          plain: true,
-          text: false
+          title: "Intelligent Systems",
+          to: "/teams/intsys"
         },
         {
-          name: 'Recruitment',
-          to: '/recruitment',
-          plain: true,
-          text: false
+          title: "Data Engineering",
+          to: "/teams/de"
         },
         {
-          name: 'Teams',
-          to: '/teams',
-          plain: true,
-          text: false
+          title: "Insights",
+          to: "/teams/insights"
         },
         {
-          name: 'People',
-          to: '/people',
-          plain: true,
-          text: false
+          title: "Algorithm Trading",
+          to: "/teams/algo"
         },
         {
-          name: 'Education',
-          to: '/education',
-          plain: true,
-          text: false
-        },
-        {
-          name: 'Blog',
-          to: '/blog',
-          plain: true,
-          text: false
-        },
+          title: "Education",
+          to: "/teams/edu"
+        }
       ],
-      title: 'Cornell Data Science'
     }
   }
 }
