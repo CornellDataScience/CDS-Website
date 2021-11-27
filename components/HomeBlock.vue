5f8252798a44ea1a4fb7a5960939246a6834a046
ejl242 <ejl242@cornell.edu>
2021-11-27 01:03:47 -0500

    Home page improved

--- components/HomeBlock.vue
+++ components/HomeBlock.vue
@@ -1,22 +1,22 @@
 <template>
     <div>
         <v-row class="mt-4 text-center">
-            <v-col class = "flex-grow-0 mx-auto">
+            <v-col >
                 <div v-if="index % 2">
-                    <p class="flex-grow-0 text-h5 font-weight-medium mx-md-8"> {{ title }}</p>
-                    <p class="flex-grow-0 font-weight-small mx-md-n16"> {{ data }}</p>
+                    <p class="text-h5 font-weight-medium mx-md-8"> {{ title }}</p>
+                    <p class="font-weight-small mx-md-n16"> {{ data }}</p>
                 </div>
                 <div v-else>
-                    <img class="flex-grow-0" :src="require(`../assets/img/${img}`)" height="200" width="300">
+                    <img :src="require(`../assets/img/${img}`)" height="200" width="300">
                 </div>
             </v-col>
-            <v-col class = "flex-grow-0 mx-auto">
+            <v-col >
                 <div v-if="!(index % 2)">
-                    <p class="flex-grow-0 text-h5 font-weight-medium mx-md-8"> {{ title }}</p>
-                    <p class="flex-grow-0 font-weight-small mx-md-n16"> {{ data }}</p> 
+                    <p class="text-h5 font-weight-medium mx-md-8"> {{ title }}</p>
+                    <p class="font-weight-small mx-md-n16"> {{ data }}</p> 
                 </div>
                 <div v-else>
-                    <img class="flex-grow-0" :src="require(`../assets/img/${img}`)" height="200" width="300">
+                    <img :src="require(`../assets/img/${img}`)" height="200" width="300">
                 </div>
             </v-col>
         </v-row>
--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,41 @@
 <template>
   <div>
-    <v-img :src="require(`../assets/img/${data.img}`)" :lazy-src="require(`../assets/img/${data.img}`)" width="2500" height="650">
-        <p class="text-h3 font-weight-medium text-uppercase text-center" style="color:white; position:relative; top:300px;">
-            {{ data.imgText }}
-        </p>
-    </v-img>
-    <p class="text-h3 font-weight-medium text-uppercase text-center mt-16 primary--text">
-        {{ data.title }}
-    </p>
-    <div class="mx-16 my-8">
-        <p class="body-1 font-weight-light mx-md-18">
-        {{ data.description }}
-        </p>
+    <div>
+      <v-img :src="require(`../assets/img/${data.img}`)" :lazy-src="require(`../assets/img/${data.img}`)" 
+      gradient="rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3)" :aspect-ratio="16/9" class="align-center">
+          <p class="mx-8 d-flex justify-center text-h2 white--text font-weight-medium text-uppercase title">
+              {{ data.imgText }}
+          </p>
+      </v-img>
+      <p class="text-h3 font-weight-medium text-uppercase text-center mt-16 primary--text">
+          About
+      </p>
+    </div>
+    <v-container class="my-8">
+        <v-row justify="center">
+            <v-col cols="9">
+                <p class="body-1 font-weight-light mx-md-18">
+                {{ data.description }}
+                </p> 
+            </v-col>
+        </v-row>
+        <v-spacer></v-spacer>
         <br>
-        <hr>
-        <div v-for="(item, index) in data.info" v-bind:key="item.title">
-        <HomeBlock v-bind:title="item.title" v-bind:index="index" v-bind:data="item.body" v-bind:img="item.img"></HomeBlock>
-        </div>
+        <v-row v-for="(item, index) in data.info" v-bind:key="item.title" justify="center">
+            <v-col cols="9">
+                <HomeBlockAlt v-bind:title="item.title" v-bind:index="index" v-bind:data="item.body" v-bind:img="item.img"></HomeBlockAlt>
+            </v-col>
+        </v-row>
         <br>
-        <hr>
         <p class="text-h5 body-2 font-weight-medium text-uppercase text-center mt-8" style="color: grey;">
-            {{ data.sponsorHeader }}
+            A special thanks to our sponsors:
         </p>
-        <div style="display: grid; grid-template-columns: repeat(auto-fill, minmax(10rem, 20rem)); align-items: center;" class="center">
-            <div v-for="img in data.sponsorImages" v-bind:key="img">
-                <img :src="require(`../assets/img/${img}`)" width="75%">
+        <div style="display: grid; grid-template-columns: repeat(auto-fill, minmax(10rem, 10rem)); align-items: center;" class="center justify-center mx-16">
+            <div v-for="img in data.sponsorImages" v-bind:key="img" class="center">
+                <v-img :src="require(`../assets/img/${img}`)" :aspect-ratio="1" class="justify-center mx-4 my-4" contain></v-img>
             </div>
         </div>
-    </div>
+    </v-container>
   </div>
 </template>
 
@@ -42,3 +50,9 @@
     }
 </script>
 
+<style scoped>
+.title {
+    position:relative; 
+    /* top:300px; */
+}
+</style>
