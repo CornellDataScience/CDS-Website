816c47b6e470459d5f980b4edb6723c89be8fc3c
mbulling <42184555+mbulling@users.noreply.github.com>
2022-02-05 17:14:03 -0500

    fixing logos1

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <v-img :src="require(`../assets/img/${data.img}`)" :lazy-src="require(`../assets/img/${data.img}`)" 
       gradient="rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3)" class="align-center">
-          <p class="mx-8 d-flex justify-center text-h2 white--text font-weight-medium text-uppercase title">
+          <p class="mx-2 d-flex justify-center text-h2 white--text font-weight-small text-uppercase title">
               {{ data.imgText }}
           </p>
       </v-img>
