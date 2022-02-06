e4519bf93608d6c764740076abfeb3ac817ff68e
David Han <dmh338@cornell.edu>
2022-02-05 21:33:42 -0500

    Changed columns to cards for section headers

--- components/HomeBlockAlt.vue
+++ components/HomeBlockAlt.vue
@@ -1,20 +1,15 @@
 <template>
     <div>
-        <div class="d-flex flex-row flex-wrap flex-md-nowrap mt-4 text-center justify-center align-center">
-            <div v-if="index % 2" class="justify-center mx-4 mt-4 order-2">
-                <p class="text-h5 font-weight-medium mx-md-8"> {{ title }}</p>
-                <p class="font-weight-small "> {{ data }}</p>
-            </div>
-            <div v-else class="order-1">
-                <img :src="require(`../assets/img/${img}`)" class="mx-4" height="240" width="360">
-            </div>
-            <div v-if="!(index % 2)" class="justify-center mx-4 mt-4 order-2 order-md-1">
-                <p class="text-h5 font-weight-medium mx-md-8"> {{ title }}</p>
-                <p class="font-weight-small "> {{ data }}</p> 
-            </div>
-            <div v-else class="order-1 order-md-2">
-                <img :src="require(`../assets/img/${img}`)" class="mx-4" height="240" width="360">
-            </div>
+        <div class="d-flex flex-wrap flex-md-nowrap mt-4 text-center justify-center align-center">
+            <v-card elevation="5" height="500px">
+                <div class="justify-center mt-4 order-2">
+                    <v-card-title class="text-h5 font-weight-medium mx-md-8 align-center"> {{ title }}</v-card-title>
+                    <v-card-text class="font-weight-small"> 
+                        {{ data }}
+                    </v-card-text>
+                    <v-img :src="require(`../assets/img/${img}`)" class="mx-4 contain" height="200px"></v-img>
+                </div>
+            </v-card>
         </div>
     </div>
 </template>
