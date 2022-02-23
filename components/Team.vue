86be03f8fe3d493351e96ff51207184e900d7f5c
Elias Little <eliasjlittle@gmail.com>
2022-02-23 18:33:34 -0500

    Added cards for projects

--- components/Team.vue
+++ components/Team.vue
@@ -4,13 +4,13 @@
           {{ title }}
       </p>
       <div class="mx-16 my-8">
-          <p class="body-1 font-weight-light mx-md-8">
+          <p v-if="leads.length !== 0" class="body-1 font-weight-light mx-md-8">
                 <b class="font-weight-bold">Team Leads: </b> 
                 <span v-for="(lead, index) in leads" v-bind:key="lead.name">
                     <a v-bind:href="lead.link" class="text-decoration-none">{{ lead.name }}</a><span v-if="index != Object.keys(leads).length - 1">, </span>
                 </span>
           </p>
-          <p class="body-1 font-weight-light mx-md-8">
+          <p v-if="advisors.length !== 0" class="body-1 font-weight-light mx-md-8">
                 <b class="font-weight-bold">Advisor: </b> 
                 <span v-for="(advisor, index) in advisors" v-bind:key="advisor.name">
                     <a v-bind:href="advisor.link" class="text-decoration-none">{{ advisor.name }}</a><span v-if="index != Object.keys(advisors).length -1">, </span>
