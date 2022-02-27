c8c49f5a31aab99124d5c130e90c5eefdd59183a
Elias Little <eliasjlittle@gmail.com>
2022-02-26 19:05:18 -0500

    Added optional link to block title

--- components/Block.vue
+++ components/Block.vue
@@ -3,7 +3,8 @@
         <v-sparkline :line-width="0.5" :gradient="['grey']" :height="10" :padding="2" :smooth="10" :value="[1, 1]" auto-draw class="mb-4"></v-sparkline>
         <v-row class="mt-4">
             <v-col cols="12" md="4">
-                <p class="flex-grow-0 text-h4 font-weight-medium mx-md-8 text-uppercase primary--text"> {{ title }} </p>
+                <a v-if="link" class="flex-grow-0 text-h4 font-weight-medium mx-md-8 text-uppercase primary--text text-decoration-none" :href="link"> {{ title }} </a>
+                <p v-else class="flex-grow-0 text-h4 font-weight-medium mx-md-8 text-uppercase primary--text text-decoration-none"> {{ title }} </p>
             </v-col>
             <v-col cols="12" md="8">
                 <slot></slot>
@@ -16,6 +17,7 @@
     export default {
         props: {
             title: String,
+            link: String
         }
     }
 </script>
