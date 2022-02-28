cb1220bcd5cc77ecdb76662c87d4b9babbd3270a
Elias Little <eliasjlittle@gmail.com>
2022-02-28 16:13:40 -0500

    Added sparkline back

--- pages/teams/index.vue
+++ pages/teams/index.vue
@@ -3,7 +3,7 @@
       <p class="text-h3 font-weight-medium text-uppercase text-center mt-16">
           {{ data.title }}
       </p>
-      <div class="mx-16 my-8">
+      <div class="mx-16 pa-8 my-8">
           <p class="body-1 font-weight-light mx-md-8">
             {{ data.description }}
           </p>
@@ -14,6 +14,7 @@
             {{ data.subdescription }}
           </p>
           <div v-for="item in data.info" v-bind:key="item.title">
+            <v-sparkline :line-width="0.5" :gradient="['grey']" :height="10" :padding="2" :smooth="10" :value="[1, 1]" auto-draw class="mb-4"></v-sparkline>
             <Block v-bind:title="item.name" v-bind:link="item.link"><p v-html="item.description"></p></Block>
           </div>
       </div>
