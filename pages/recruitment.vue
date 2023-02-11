6114b136d9c2ad0b908b87b04b5fc372e7665568
David Han <dmh338@cornell.edu>
2023-02-10 22:26:17 -0500

    Update photos for individuals; update recruiting timeline

--- pages/recruitment.vue
+++ pages/recruitment.vue
@@ -12,9 +12,6 @@
             <FloatingCard :link="item.link">
               <Block v-bind:title="item.title">
                 <p v-html="item.body"></p>
-                <v-btn :href="item.linkMLE" v-if="item.title === 'Application'">MLE Application</v-btn>
-                <v-btn :href="item.linkDS" v-if="item.title === 'Application'">DS Application</v-btn>
-                <v-btn :href="item.linkQF" v-if="item.title === 'Application'">QF Application</v-btn>
               </Block>
             </FloatingCard>
           </v-timeline-item>
@@ -22,9 +19,6 @@
           <FloatingCard v-for="item in data.info" v-bind:key="item.title" class="hidden-md-and-up">
               <Block v-bind:title="item.title">
                 <p v-html="item.body"></p>
-                <v-btn :href="item.linkMLE" v-if="item.title === 'Application'">MLE Application</v-btn>
-                <v-btn :href="item.linkDS" v-if="item.title === 'Application'">DS Application</v-btn>
-                <v-btn :href="item.linkQF" v-if="item.title === 'Application'">QF Application</v-btn>
             </Block>
           </FloatingCard>
       </div>
