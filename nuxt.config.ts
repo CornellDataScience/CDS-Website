96f8dc1c93a6e655ee589138d3ef7814bd1425de
Bryant Park <bryantpark04@gmail.com>
2023-07-22 03:06:00 -0400

    Migrate head from old project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,4 +10,20 @@ export default defineNuxtConfig({
       'process.env.DEBUG': false,
     },
   },
+  app: {
+    head: {
+      titleTemplate: '%s | Cornell Data Science',
+      title: 'Home',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
 })
