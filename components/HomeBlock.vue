1e50c80ecc5be84833803f6d440b2a1bb6066d3a
Bryant Park <bryantpark04@gmail.com>
2023-07-22 03:17:06 -0400

    Copy components from old project
    They might be broken

---
+++ components/HomeBlock.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <v-row class="mt-4 text-center">
+            <v-col >
+                <div v-if="index % 2">
+                    <p class="text-h5 font-weight-medium mx-md-8"> {{ title }}</p>
+                    <p class="font-weight-small mx-md-n16"> {{ data }}</p>
+                </div>
+                <div v-else>
+                    <img :src="require(`../assets/img/${img}`)" height="200" width="300">
+                </div>
+            </v-col>
+            <v-col >
+                <div v-if="!(index % 2)">
+                    <p class="text-h5 font-weight-medium mx-md-8"> {{ title }}</p>
+                    <p class="font-weight-small mx-md-n16"> {{ data }}</p> 
+                </div>
+                <div v-else>
+                    <img :src="require(`../assets/img/${img}`)" height="200" width="300">
+                </div>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            title: String,
+            index: Number,
+            data: String,
+            img: String
+        }
+    }
+</script>
---
+++ components/Navbar.vue
@@ -0,0 +1,142 @@
+<template>
+    <v-app-bar>
+      <NuxtLink to="/">
+        <img contain src="/cds_logo.png" aspect-ratio="1" width="50px" align="center" />
+      </NuxtLink>
+      <NuxtLink to="/" style="text-decoration:none">
+        <!-- <v-toolbar-title v-text="title" class="justify-center text-p font-weight-small text-uppercase" /> -->
+      </NuxtLink>
+      <v-spacer />
+
+      <!-- Full Nav Bar -->
+      <div class="hidden-sm-and-down">
+        <v-menu
+          open-on-hover
+          bottom
+          offset-y
+        >
+          <template v-slot:activator="{ on }">
+            <v-btn plain small class="nav-btn mx-2 body-2 font-weight-medium" active-class="primary--text" :to="'/teams'" v-on="on" nuxt>
+              Teams
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="(item, index) in teams" :key="index">
+              <v-list-item-title>
+                <v-btn plain class="nav-btn mx-2 body-2 font-weight-medium" active-class="primary--text" v-bind:to="item.to" v-on="on" nuxt>
+                  {{ item.title }}
+                </v-btn>
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+        <v-btn plain small class="nav-btn mx-2 body-2 font-weight-medium" active-class="primary--text" href="/projects">
+          Projects
+        </v-btn>
+        <v-btn plain small class="nav-btn mx-2 body-2 font-weight-medium" active-class="primary--text" :to="'/education'" nuxt>
+          Education
+        </v-btn>
+        <v-btn plain small class="nav-btn mx-2 body-2 font-weight-medium" active-class="primary--text" href="https://cornelldatascience.github.io/education-blogs/">
+          Blog
+        </v-btn>
+        <v-btn plain small class="nav-btn mx-2 body-2 font-weight-medium" active-class="primary--text" href="/contact">
+          Contact
+        </v-btn>
+        <v-btn text small class="nav-btn mx-2 navColor white--text body-2 font-weight-medium rounded-xl" to="/recruitment" nuxt>
+          Apply
+        </v-btn>
+      </div>
+
+      <!-- Mobile Nav Bar -->
+      <v-menu open-on-click bottom offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn plain class="hidden-md-and-up nav-btn body-2 font-weight-medium" active-class="primary--text" v-on="on">
+            <v-icon large >mdi-menu</v-icon>
+          </v-btn>
+        </template>
+        
+        <v-list nav>
+          <v-list-item>
+            <v-menu offset-y>
+              <template v-slot:activator="{ on }">
+                <v-btn plain x-small class="nav-btn body-2 font-weight-medium" active-class="primary--text" v-on="on">
+                  Teams
+                </v-btn>
+              </template>
+
+              <v-list nav>
+                <v-list-item>
+                  <v-list-item-title>
+                    <v-btn plain x-small class="nav-btn body-2 font-weight-medium" active-class="primary--text" v-bind:to="'/teams'" v-on="on" nuxt>
+                      Overview
+                    </v-btn>
+                  </v-list-item-title>
+                </v-list-item>
+                <v-list-item v-for="(item, index) in teams" :key="index">
+                  <v-list-item-title>
+                    <v-btn plain x-small class="nav-btn body-2 font-weight-medium" active-class="primary--text" v-bind:to="item.to" v-on="on" nuxt>
+                      {{ item.title }}
+                    </v-btn>
+                  </v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-list-item>
+          <v-list-item>
+            <v-btn plain x-small class="nav-btn body-2 font-weight-medium" active-class="primary--text" href="/projects">
+              Projects
+            </v-btn>
+          </v-list-item>
+          <v-list-item>
+            <v-btn plain x-small class="nav-btn body-2 font-weight-medium" active-class="primary--text" :to="'/education'" nuxt>
+              Education
+            </v-btn>
+          </v-list-item>
+          <v-list-item>
+            <v-btn plain x-small class="nav-btn body-2 font-weight-medium" active-class="primary--text" href="https://cornelldatascience.github.io/education-blogs/">
+              Blog
+            </v-btn>
+          </v-list-item>
+          <v-list-item>
+            <v-btn text x-small class="nav-btn navColor body-2 font-weight-medium" to="/recruitment" nuxt>
+              Apply
+            </v-btn>
+          </v-list-item>
+
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+</template>
+
+<script>
+export default ({
+    data () {
+        return {
+        hover: false,
+        title: 'Cornell Data Science',
+        teams: [
+            {
+            title: "Executive Board",
+            to: "/teams/eboard"
+            },
+            {
+            title: "Data Science",
+            to: "/teams/ds"
+            },
+            {
+            title: "Data Engineering",
+            to: "/teams/de"
+            },
+            {
+            title: "Machine Learning Engineering",
+            to: "/teams/mle"
+            },
+            {
+            title: "Quantitative Finance",
+            to: "/teams/qf"
+            }
+        ],
+        }
+    }
+})
+</script>
