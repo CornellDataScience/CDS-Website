11674591c77427ad53f2cf3d8cfc8c63f362fa2e
Bryant Park <bryantpark04@gmail.com>
2023-07-23 18:28:55 -0400

    Add and fix FloatingCard and index.vue

---
+++ components/FloatingCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- Do I need to use props in v-bind here? The API changed in Vue 3 from one variable to two. There's also another place where I did this change -->
+  <v-hover v-slot:default="{ isHovering, props }">
+    <v-card rounded v-if="link" :href="link" :elevation="isHovering ? 12 : 2" class="rounded-xl pa-md-4 my-8">
+      <slot></slot>
+    </v-card>
+    <v-card rounded v-else :elevation="isHovering ? 12 : 2" class="rounded-xl pa-md-4 my-8">
+      <slot></slot>
+    </v-card>
+  </v-hover>
+</template>
+
+<script setup lang="ts">
+defineProps({ link: String })
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,96 @@
 <template>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, doloribus alias aperiam, cumque fugit voluptas
-    explicabo assumenda repudiandae delectus dolorem sapiente, minima eveniet fugiat dolore saepe. Itaque tempora odit
-    optio.</p>
+  <div>
+    <div>
+      <v-parallax width="100%" :src="home.img" :lazy-src="home.img" gradient="rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)"
+        height="750" class="align-center">
+        <p class="mx-2 d-flex justify-center text-h2 white--text font-weight-normal text-uppercase title">Cornell
+          Data
+          Science</p>
+      </v-parallax>
+      <p class="text-h3 justify-center font-weight-medium text-uppercase text-center mt-16 primary--text">
+        About
+      </p>
+    </div>
+    <v-container class="my-8 px-8 px-md-0">
+      <v-row justify="center">
+        <v-col cols="12" md="9">
+          <p class="body-1 text--black font-weight-light ml-4 mx-md-18">
+            {{ home.description }}
+          </p>
+        </v-col>
+      </v-row>
+      <v-spacer></v-spacer>
+      <br>
+      <v-row>
+        <v-col cols="12" sm="6" md="4" height="100%" v-for="(item, index) in  home.info " v-bind:key="item.title"
+          justify="center">
+          <v-hover v-slot:default="{ isHovering, props }">
+            <v-card shaped class="rounded-xl py-4 d-flex flex-column" :to="item.linker" height="100%"
+              :elevation="isHovering ? 12 : 2">
+              <v-card-title class="text-h5 text-center font-weight-medium mx-md-8 justify-center primary--text"> {{
+                item.title }}</v-card-title>
+              <v-card-text class="font-weight-small">
+                {{ item.body }}
+              </v-card-text>
+              <v-img :src="item.img" max-height="200" class="mt-auto rounded-xl mx-4" height="200px"></v-img>
+            </v-card>
+          </v-hover>
+          <!--<HomeBlockAlt v-bind:title="item.title" v-bind:index="index" v-bind:data="item.body" v-bind:img="item.img"></HomeBlockAlt>-->
+        </v-col>
+      </v-row>
+      <FloatingCard link="/teams">
+        <Block title="Teams">
+          <template v-slot:leftContent>
+            <p>{{ teams.description }}</p>
+          </template>
+          <v-img aspect-ratio="3" class="rounded-xl" :src="teamsImage"></v-img>
+        </Block>
+      </FloatingCard>
+      <FloatingCard link="/teams/eboard">
+        <Block title="People">
+          <template v-slot:leftContent>
+            <p>{{ home.people }}</p>
+          </template>
+          <v-img aspect-ratio="2.5" class="rounded-xl" :src="eboardImage"></v-img>
+        </Block>
+      </FloatingCard>
+      <FloatingCard link="/education">
+        <Block title="Education">
+          <template v-slot:leftContent>
+            <p>{{ home.education }}</p>
+          </template>
+          <v-img aspect-ratio="3" class="rounded-xl" :src="educationImage"></v-img>
+        </Block>
+      </FloatingCard>
+
+      <br>
+      <p class="text-h5 body-2 font-weight-medium text-uppercase text-center mt-8" style="color: grey;">
+        A special thanks to our sponsors:
+      </p>
+      <div style="display: grid; grid-template-columns: repeat(auto-fill, minmax(10rem, 10rem)); align-items: center;"
+        class="center justify-center mx-16" align="center" justify="center">
+        <div v-for=" img  in  home.sponsorImages " v-bind:key="img" class="center">
+          <v-img :src="img" :aspect-ratio="1" class="justify-center mx-4 my-4" contain></v-img>
+        </div>
+      </div>
+    </v-container>
+  </div>
 </template>
 
 <script setup lang="ts">
+import home from '~/assets/json/home.json';
+import teams from '~/assets/json/teams.json';
+
+home.img = new URL(`../assets/img/${home.img}`, import.meta.url).href
+home.sponsorImages = home.sponsorImages.map((img) => new URL(`../assets/img/${img}`, import.meta.url).href)
+home.info = home.info.map((info) => {
+  return { ...info, img: new URL(`../assets/img/${info.img}`, import.meta.url).href }
+})
+
+const educationImage = new URL(`../assets/img/info1998_2.png`, import.meta.url).href
+const eboardImage = new URL(`../assets/img/people.jpg`, import.meta.url).href
+const teamsImage = new URL(`../assets/img/home1.jpg`, import.meta.url).href
+
 useHead({
   title: 'Cornell Data Science',
   titleTemplate: null,
@@ -12,7 +98,8 @@ useHead({
 </script>
 
 <style scoped>
-p {
-  color: black;
+.title {
+  position: relative;
+  /* top:300px; */
 }
 </style>
