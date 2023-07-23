39bd11ffacadee7c6f66389d4450837ec8691df2
Bryant Park <bryantpark04@gmail.com>
2023-07-23 17:25:04 -0400

    Add and fix eboard page
    - Remove dynamic imports - they don't work in Vue 3 anymore.
    I can't believe the error handling was so bad...
    THE DYNAMIC REQUIRE WAS THE GODDAMN ISSUE
    
    - Move image to public folder

---
+++ pages/teams/eboard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="px-4">
+    <p class="text-h3 font-weight-medium text-uppercase text-center mt-16">
+      {{ eboard.title }}
+    </p>
+    <div class="mx-md-16 my-8">
+      <v-row justify="center" class="mx-md-16">
+        <v-col cols="12" sm="4" md="3" xl="2" v-for="member in eboard.people" v-bind:key="member.name"
+          class="text-center">
+          <v-card class="rounded-xl mx-auto" height="200" width="200">
+            <v-img :src="`/img/headshots/${member.image}`" class="fill-height" max-height="200"></v-img>
+          </v-card>
+          <b>
+            <p class="mt-2">{{ member.title }}</p>
+          </b>
+          <p class="mt-2">{{ member.name }}</p>
+        </v-col>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import eboard from '../../assets/json/teams/eboard.json';
+
+useHead({ title: 'Executive Board' })
+</script>
+
