fb43f78f046f8b01a67dffebb1f407d6ad900d38
Bryant Park <bryantpark04@gmail.com>
2023-07-24 01:38:51 -0400

    Add CDS themes (broken) and set default to cdsDark
    Enable SSR

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,12 +1,39 @@
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import colors from 'vuetify/lib/util/colors'
+
+const cdsLight = {
+  dark: false,
+  primary: colors.deepPurple,
+  navColor: colors.deepPurple,
+  secondary: '#595B86',
+  accent: '#F3A93C',
+}
+const cdsDark = {
+  dark: true,
+  primary: colors.deepPurple.lighten3,
+  navColor: colors.deepPurple,
+  accent: '#F3A93C',
+  secondary: '#8587AD',
+  info: colors.teal.lighten1,
+  warning: colors.amber.base,
+  error: colors.deepOrange.accent4,
+  success: colors.green.accent3,
+}
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     ssr: true,
     components,
     directives,
+    theme: {
+      defaultTheme: 'cdsDark',
+      themes: {
+        cdsLight,
+        cdsDark,
+      },
+    },
   })
 
   nuxtApp.vueApp.use(vuetify)
