4fe3d16de0e73dbfe9aebd3c3c352135de4fe939
Bryant Park <bryantpark04@gmail.com>
2023-07-24 01:42:22 -0400

    Fix sponsorship packet display and layout issue

--- pages/contact.vue
+++ pages/contact.vue
@@ -28,19 +28,19 @@
         <template v-slot:title>
           Sponsorship <br /> Packet
         </template>
-        <v-expansion-panels class="">
+        <v-expansion-panels>
           <v-expansion-panel>
-            <v-expansion-panel-header>
+            <v-expansion-panel-title>
               <div class="text-h4 secondary--text">Open to View</div>
-            </v-expansion-panel-header>
-            <v-expansion-panel-content class="d-flex justify-center px-auto">
+            </v-expansion-panel-title>
+            <v-expansion-panel-text class="d-flex justify-center px-auto">
               <v-row justify="center">
-                <iframe class="mx-auto"
+                <iframe class="mx-auto mt-2"
                   src="https://docs.google.com/presentation/d/e/2PACX-1vRs4rrovYiIcWlG3ByrGVFu29-JNx_94b4JD_h7W5z8llzwr-eH94KzAuumMIgi6-k2Kl5if6-NX4AD/embed?start=true&loop=false&delayms=3000"
                   frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true"
                   webkitallowfullscreen="true"></iframe>
               </v-row>
-            </v-expansion-panel-content>
+            </v-expansion-panel-text>
           </v-expansion-panel>
         </v-expansion-panels>
         <div class="my-4 mx-md-8">
