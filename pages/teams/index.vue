1096c70fb6cf455b7e2e99a55a72e16b293af14e
Bryant Park <bryantpark04@gmail.com>
2023-07-24 01:04:19 -0400

    Add and fix teams page

---
+++ pages/teams/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p class="text-h3 font-weight-medium text-uppercase text-center mt-16">
+      {{ teams.title }}
+    </p>
+    <div class="mx-md-16 pa-8 my-8">
+      <p class="body-1 font-weight-light mx-md-8">
+        {{ teams.description }}
+      </p>
+      <p class="text-h4 primary--text font-weight-normal text-uppercase text-center mt-8">
+        {{ teams.subtitle }}
+      </p>
+      <p class="body-1 font-weight-light mx-md-8">
+        {{ teams.subdescription }}
+      </p>
+      <div v-for="item in teams.info" v-bind:key="item.name">
+        <v-sparkline :line-width="0.5" :gradient="['grey']" :height="10" :padding="2" :smooth="10" :value="[1, 1]"
+          auto-draw class="mb-4" />
+        <Block v-bind:title="item.name" v-bind:link="item.link">
+          <p v-html="item.description"></p>
+        </Block>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import teams from '~/assets/json/teams.json';
+
+useHead({ title: 'Teams' })
+</script>
+
